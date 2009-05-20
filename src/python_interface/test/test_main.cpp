36232dae4d69a6ebd17dec8df10ce832eba2641a
loonycyborg <loonycyborg@localhost>
2009-05-20 22:43:46 +0000

    Register an exception translator for boost::python::error_already_set in unit test monitor.

--- src/python_interface/test/test_main.cpp
+++ src/python_interface/test/test_main.cpp
@@ -1,15 +1,24 @@
 #define BOOST_TEST_MODULE scons_plus_plus regression tests
 #include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_monitor.hpp>
 
 #include "python_interface/python_interface.hpp"
 #include "python_interface/python_interface_internal.hpp"
 #include "test_utils.hpp"
+
+static void translate_error_already_set(const error_already_set&)
+{
+	PyErr_Print();
+	PyErr_Clear();
+	throw std::runtime_error("A python exception was thrown");
+}
 
 struct python_setup
 {
 	python_setup()
 	{
 		python_interface::init_python();
+		boost::unit_test::unit_test_monitor.register_exception_translator<error_already_set>(&translate_error_already_set);
 	}
 };
 
