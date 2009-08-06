f09f90ee0378c483918ae0cad787a25e2d0005a2
loonycyborg <loonycyborg@localhost>
2009-08-06 17:27:08 +0000

    Use boost.multi_index to implement task list in taskmaster instead of a std::set and a std::vector.

--- src/taskmaster.cpp
+++ src/taskmaster.cpp
@@ -20,6 +20,13 @@
 
 #include <boost/graph/topological_sort.hpp>
 #include <boost/foreach.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_bind.hpp>
 #include <map>
 #define foreach BOOST_FOREACH
 
@@ -30,19 +37,61 @@ using std::vector;
 using boost::depth_first_visit;
 using boost::associative_property_map;
 
+using boost::multi_index_container;
+using boost::multi_index::indexed_by;
+using boost::multi_index::tag;
+using boost::multi_index::sequenced;
+using boost::multi_index::ordered_unique;
+
+using boost::tie;
+
+using boost::phoenix::arg_names::arg1;
+using boost::phoenix::push_back;
+using boost::phoenix::bind;
+
 using dependency_graph::Node;
+using dependency_graph::NodeList;
+
+struct TaskListItem
+{
+	boost::shared_ptr<taskmaster::Task> task;
+	NodeList targets;
+
+	TaskListItem(boost::shared_ptr<taskmaster::Task> task, Node target) : task(task) { targets.push_back(target); }
+};
+
+struct sequence_index;
+struct task_index;
+
+typedef multi_index_container<
+	TaskListItem,
+	indexed_by<
+		sequenced<tag<sequence_index> >,
+		ordered_unique<tag<task_index>, BOOST_MULTI_INDEX_MEMBER(TaskListItem, boost::shared_ptr<taskmaster::Task>, task)>
+		>
+	> TaskList;
 
 class BuildVisitor : public boost::default_dfs_visitor
 {
-	std::vector<Node>& node_list_;
+	TaskList& task_list_;
+	std::vector<Node>& build_order_;
+
 	public:
-	BuildVisitor(std::vector<Node>& node_list) : node_list_(node_list) {}
+	BuildVisitor(TaskList& task_list, std::vector<Node>& build_order) : task_list_(task_list), build_order_(build_order) {}
 
 	template <typename Edge>
 	void back_edge(const Edge&, const dependency_graph::Graph& graph) const { throw boost::not_a_dag(); }
 	void finish_vertex(Node node, const dependency_graph::Graph& graph) const
 	{
-		node_list_.push_back(node);
+		boost::shared_ptr<taskmaster::Task> task = graph[node]->task();
+		if(task) {
+			bool inserted;
+			TaskList::iterator iter;
+			tie(iter, inserted) = task_list_.push_back(TaskListItem(task, node));
+			if(!inserted)
+				task_list_.modify(iter, push_back(bind(&TaskListItem::targets, arg1), node));
+		}
+		build_order_.push_back(node);
 	}
 };
 
@@ -65,34 +114,30 @@ namespace
 
 namespace taskmaster
 {
-	void build_order(dependency_graph::Node end_goal, std::vector<Node>& output)
+	void build_order(dependency_graph::Node end_goal, TaskList& tasks, std::vector<Node>& output)
 	{
 		std::map<Node, boost::default_color_type> colors;
 		associative_property_map<std::map<Node, boost::default_color_type> > color_map(colors);
-		depth_first_visit(dependency_graph::graph, end_goal, BuildVisitor(output), color_map);
+		depth_first_visit(dependency_graph::graph, end_goal, BuildVisitor(tasks, output), color_map);
+	}
+
+	void build_order(dependency_graph::Node end_goal, std::vector<Node>& output)
+	{
+		TaskList tasks;
+		build_order(end_goal, tasks, output);
 	}
 
 	void build(dependency_graph::Node end_goal)
 	{
+		TaskList tasks;
 		std::vector<Node> nodes;
-		build_order(end_goal, nodes);
-
-		std::vector<boost::shared_ptr<taskmaster::Task> > tasks;
-		std::set<boost::shared_ptr<taskmaster::Task> > added_tasks;
-		for(vector<dependency_graph::Node>::const_iterator iter = nodes.begin(); iter != nodes.end(); iter++)
-		{
-			boost::shared_ptr<taskmaster::Task> task = dependency_graph::graph[*iter]->task();
-			if(task && !added_tasks.count(task)) {
-				tasks.push_back(task);
-				added_tasks.insert(task);
-			}
-		}
+		build_order(end_goal, tasks, nodes);
 
 		const int num_tasks = tasks.size();
 		int current_task = 1;
-		foreach(boost::shared_ptr<taskmaster::Task> task, tasks) {
-			std::cout << "[" << current_task++ << "/" << num_tasks << "] " << task->targets() << " <- " << task->sources() << "\n";
-			task->execute();
+		foreach(const TaskListItem& item, tasks.get<sequence_index>()) {
+			std::cout << "[" << current_task++ << "/" << num_tasks << "] " << item.task->targets() << " <- " << item.task->sources() << "\n";
+			item.task->execute();
 		}
 	}
 
