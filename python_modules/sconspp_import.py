9b12f056f7cabdb2172eaa10e9285f315618ca92
loonycyborg <loonycyborg@localhost>
2009-09-22 22:29:48 +0000

    Fixed sconspp_import.py breakage with python 2.6

--- python_modules/sconspp_import.py
+++ python_modules/sconspp_import.py
@@ -3,13 +3,13 @@ import __builtin__
 old_import = __builtin__.__import__
 import SCons, sys
 
-def sconspp_import(name, globals=None, locals=None, fromlist=None, blah=None):
+def sconspp_import(name, globals=None, locals=None, fromlist=None, blah=None, **kw):
     try:
-        return old_import(name, globals, locals, fromlist)
+        return old_import(name, globals, locals, fromlist, **kw)
     except ImportError:
         module = name.split(".")
         if module[0] == "SCons":
-            old_import(".".join(module[1:]), globals, locals, fromlist)
+            old_import(".".join(module[1:]), globals, locals, fromlist, **kw)
             setattr(SCons, module[1], old_import(module[1]))
             sys.modules["SCons." + module[1]] = old_import(module[1])
             return SCons
