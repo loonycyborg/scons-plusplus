f4a2474f5b7cf4a7c210f120c52b9b6c1c709445
loonycyborg <loonycyborg@localhost>
2009-09-23 15:34:49 +0000

    Silence weird DeprecationWarnings that happen with python 2.6.2 when calling __builtin__.__import__ directly.

--- python_modules/sconspp_import.py
+++ python_modules/sconspp_import.py
@@ -2,6 +2,10 @@
 import __builtin__
 old_import = __builtin__.__import__
 import SCons, sys
+import warnings
+
+# With python 2.6.2 calling __builtin__.__import__ manually causes DeprecationWarnings about md5 and popen2
+warnings.filterwarnings('ignore', category = DeprecationWarning, module = 'sconspp_import')
 
 def sconspp_import(name, globals=None, locals=None, fromlist=None, blah=None, **kw):
     try:
